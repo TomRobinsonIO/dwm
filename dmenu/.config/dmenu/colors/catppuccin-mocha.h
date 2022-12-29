8b984ac212a950d7e7db61b9683b1bc44e4fa981
tomrobinsonio <tom@tomrobinson.io>
2022-12-29 13:13:56 -0500

    'colors'

--- dmenu/.config/dmenu/colors/catppuccin-mocha.h
+++ dmenu/.config/dmenu/colors/catppuccin-mocha.h
@@ -1,9 +1,9 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#cdd6f4", "#181825" },
-	[SchemeSel] = { "#000000", "#b4befe" },
-	[SchemeSelHighlight] = { "#a6e3a1", "#000000" },
-	[SchemeNormHighlight] = { "#a6e3a1", "#000000" },
-	[SchemeOut] = { "#000000", "#74c7ec" },
+	[SchemeSel] = { "#313244", "#b4befe" },
+	[SchemeSelHighlight] = { "#a6e3a1", "#181825" },
+	[SchemeNormHighlight] = { "#a6e3a1", "#181825" },
+	[SchemeOut] = { "#585b70", "#74c7ec" },
 	[SchemeMid] = { "#cdd6f4", "#11111b" },
 };
